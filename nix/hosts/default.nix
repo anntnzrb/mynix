568b2c7c0386ea6c3f87383091a663aba6d25248
anntnzrb <anntnzrb@protonmail.com>
2022-06-14 09:26:06 -0500

    2022-06-14 :: 09:26:06 (-05)

--- nix/hosts/default.nix
+++ nix/hosts/default.nix
@@ -3,30 +3,6 @@
 { self, lib, inputs, nixpkgs, nur, home-manager, user, ... }:
 
 let
-  hostsFolder = "${self}/nix/hosts/";
-
-  makeSystem = host: {
-    # flake variables
-    specialArgs = { inherit inputs user; };
-    modules = [
-      ./hostsFolder/host
-      ./hostsFolder/configuration.nix
-
-      home-manager.nixosModules.home-manager {
-        home-manager.useGlobalPkgs   = true;
-        home-manager.useUserPackages = true;
-        # flake variables
-        home-manager.extraSpecialArgs = { inherit user; };
-        home-manager.users.${user} = {
-          imports = [
-            ./hostsFolder/home.nix
-            ./hostsFolder/host/home.nix
-          ];
-        };
-      }
-    ];
-  };
-
   system = "x86_64-linux";
 
   lib = nixpkgs.lib;
